14ece9f075121ecdb8356832f4c0810720ad3d76
xep_drone <1479992085@qq.com>
2023-04-23 16:40:30 +0800

    add record.sh

--- shfiles/record.sh
+++ shfiles/record.sh
@@ -1,13 +1,11 @@
 rosbag record --tcpnodelay \
-/drone_0_ego_planner_node/goal_point \
-/ego_planner_node/global_list \
-/drone_0_ego_planner_node/optimal_list \
-/ego_planner_node/a_star_list \
-/drone_0_ego_planner_node/init_list \
-/drone_0_odom_visualization/path \
-/drone_0_ego_planner_node/grid_map/occupancy_inflate \
-/drone_0_odom_visualization/robot \
-/vins_fusion/path \
-/vins_fusion/odometry \
-/camera/infra1/image_rect_raw \
-/position_cmd 
+/planning_vis/trajectory \
+/planning/travel_traj \
+/planning/position_cmd_vis \
+/planning_vis/frontier \
+/planning_vis/viewpoints \
+/sdf_map/occupancy_local \
+/sdf_map/occupancy_all \
+/vins_fusion/imu_propagate 
+
+
